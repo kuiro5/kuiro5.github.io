37b8c7d2850a7449944315d6e273f93c3ed3eb23
kuiros <joshkuiros@gmail.com>
2017-08-02 20:55:18 -0700

    Font icons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 require ('./assets/stylesheets/style.scss');
+require('font-awesome-webpack');
 
--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,33 @@ let config = {
     path: path.resolve(__dirname, './public'),
     filename: 'output.js',
   },
-  resolve: { // These options change how modules are resolved
-    extensions: ['.js', '.json', '.scss', '.css', '.jpeg', '.jpg', '.gif', '.png'], // Automatically resolve certain extensions
-    alias: { // Create aliases
-      images: path.resolve(__dirname, 'src/assets/images')  // src/assets/images alias
-    }
+  resolve: {
+    // These options change how modules are resolved
+    extensions: [
+      '.js',
+      '.json',
+      '.scss',
+      '.css',
+      '.jpeg',
+      '.jpg',
+      '.gif',
+      '.png',
+    ], // Automatically resolve certain extensions
+    alias: {
+      // Create aliases
+      images: path.resolve(__dirname, 'src/assets/images'), // src/assets/images alias
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loader: ExtractTextWebpackPlugin.extract("style-loader", "css-loader")
+        loader: ExtractTextWebpackPlugin.extract('style-loader', 'css-loader'),
       },
       {
         test: /\.scss$/,
@@ -35,52 +46,58 @@ let config = {
         }))
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {  // images loader
-          loader: 'image-webpack-loader',
-          query: {
-            mozjpeg: {
-              progressive: true,
-            },
-            gifsicle: {
-              interlaced: false,
-            },
-            optipng: {
-              optimizationLevel: 4,
-            },
-            pngquant: {
-              quality: '75-90',
-              speed: 3,
+        test: /\.(jpe?g|png|gif)$/i,
+        loaders: [
+          'file-loader?context=src/assets/images/&name=images/[path][name].[ext]',
+          {
+            // images loader
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
             },
           },
-        }],
+        ],
         exclude: /node_modules/,
         include: __dirname,
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: "file-loader"
-      }
-    ]
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+    ],
   },
-  plugins: [
-    new ExtractTextWebpackPlugin('styles.css')
-  ],
+  plugins: [new ExtractTextWebpackPlugin('styles.css')],
   devServer: {
     contentBase: path.resolve(__dirname, './'),
     historyApiFallback: true,
     inline: true,
     open: true,
-    openPage: ''
+    openPage: '',
   },
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
 };
 
 module.exports = config;
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.plugins.push(
-      new webpack.optimize.UglifyJsPlugin(),
-      new OptimizeCSSAssets()
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeCSSAssets()
   );
 }
