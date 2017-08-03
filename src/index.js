c9320bcc7f4ac99c8ad0ed1534e22ef4c78c796d
kuiros <joshkuiros@gmail.com>
2017-08-02 23:30:57 -0700

    Build working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 require ('./assets/stylesheets/style.scss');
 require('font-awesome-webpack');
+//require("font-awesome-webpack!./assets/fonts/font-awesome.config.js");
 
--- webpack.config.js
+++ webpack.config.js
@@ -74,11 +74,11 @@ let config = {
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: 'url-loader?mimetype=application/font-woff?name=[hash].[ext]',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: 'file-loader?name=[hash].[ext]',
       },
     ],
   },
