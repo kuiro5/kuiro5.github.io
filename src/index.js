1c7efa842d177c65945c6c802c21add4b921a67f
kuiros <joshkuiros@gmail.com>
2018-02-11 17:04:55 -0800

    onLoad animation, move google analytics to own js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 require ('./assets/stylesheets/style.scss');
 require('font-awesome-webpack');
+require('./js/googleAnalytics.js');
+require('./js/onLoad.js');
 
---
+++ src/js/googleAnalytics.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+const gtag = () => dataLayer.push(arguments);
+gtag('js', new Date());
+gtag('config', 'UA-73908362-1');
+
