a70ded3e48cdcdc1d6c80858590b6c3b3fd698c5
Josh Kuiros <joshkuiros@gmail.com>
2018-12-04 22:10:30 -0800

    Testing deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,5 +61,23 @@ module.exports = {
         aliases: {},
       },
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
   ],
 }
